f3a1106ebc48ed2771ba1300966de3ddd8d3c46c
Daniel Auth <daniel.auth@cas.de>
2020-11-30 18:10:46 +0100

    console app and unit test project added

---
+++ AdventOfCode/AdventOfCode/Day_1/Run.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
