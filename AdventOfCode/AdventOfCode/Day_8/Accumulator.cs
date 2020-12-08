5fc3b70feeeed298d6bf87d63e88f2f8480734e6
Daniel Auth <daniel.auth@cas.de>
2020-12-08 11:40:03 +0100

    fixed endless loop

--- AdventOfCode/AdventOfCode/Day_8/Accumulator.cs
+++ AdventOfCode/AdventOfCode/Day_8/Accumulator.cs
@@ -4,72 +4,127 @@ using AdventOfCode.Utils;
 
 namespace AdventOfCode.Day_8
 {
-	public class Accumulator
-	{
-		private readonly IList<Operation> _operations;
+    public class Accumulator
+    {
+        private readonly IList<Operation> _operations;
 
-		public Accumulator(IList<Operation> operations)
-		{
-			_operations = operations;
-		}
+        public Accumulator(IList<Operation> operations)
+        {
+            _operations = operations;
+        }
 
-		public int Process(bool skipProcessed = true)
-		{
-			var index = 0;
-			int prevIndex = index;
+        //public int Process(bool skipProcessed = true)
+        //{
+        //	var index = 0;
+        //	int prevIndex = index;
 
-			var currentValue = 0;
-			var operation = _operations[index];
-			var finished = false;
+        //	var currentValue = 0;
+        //	var operation = _operations[index];
+        //	var finished = false;
 
-			var firstOperationWasProcessed = false;
+        //	var firstOperationWasProcessed = false;
 
-			do
-			{
-				if (skipProcessed)
-				{
-					finished = operation.Processed;
-				}
-
-
-				if (operation.Processed)
-				{
-					Logger.Log($"First Operation was processed at index {index}: {operation}");
-					if (firstOperationWasProcessed == false)
-					{
-						Console.ReadKey();
-						firstOperationWasProcessed = true;
-					}
+        //	do
+        //	{
+        //		if (skipProcessed)
+        //		{
+        //			finished = operation.Processed;
+        //		}
 
 
-				}
+        //		if (operation.Processed)
+        //		{
+        //			Logger.Log($"First Operation was processed at index {index}: {operation}");
+        //			if (firstOperationWasProcessed == false)
+        //			{
+        //				Console.ReadKey();
+        //				firstOperationWasProcessed = true;
+        //			}
 
-				if (!finished)
-				{
-					var next = operation.ProcessOperation(currentValue, index);
 
-					currentValue = next.Item1;
-					prevIndex = index;
-					index = next.Item2;
+        //		}
 
-					if (index >= _operations.Count)
-					{
-						Logger.Log($"index : {index}; lastIndex: {prevIndex}");
-						finished = true;
-					}
-					else
-					{
-						operation = _operations[index];
-					}
+        //		if (!finished)
+        //		{
+        //			var next = operation.ProcessOperation(currentValue, index);
 
-				}
+        //			currentValue = next.Item1;
+        //			prevIndex = index;
+        //			index = next.Item2;
 
-			} while (finished == false);
+        //			if (index >= _operations.Count)
+        //			{
+        //				Logger.Log($"index : {index}; lastIndex: {prevIndex}");
+        //				finished = true;
+        //			}
+        //			else
+        //			{
+        //				operation = _operations[index];
+        //			}
 
-			Logger.Log($"index : {index}; lastIndex: {prevIndex}");
-			operation.Print();
+        //		}
 
-			return currentValue;
-		}
-	}
+        //	} while (finished == false);
+
+        //	Logger.Log($"index : {index}; lastIndex: {prevIndex}");
+        //	operation.Print();
+
+        //	return currentValue;
+        //}
+        public int Process(bool skipProcessed = true)
+        {
+            var index = 0;
+            int prevIndex = index;
+            var currentValue = 0;
+            var operation = _operations[index];
+            bool finished;
+            var firstOperationWasProcessed = false;
+            do
+            {
+                if (skipProcessed)
+                {
+                    finished = operation.Processed;
+                }
+
+
+                if (operation.Processed)
+                {
+                    Logger.Log($"First Operation was processed at index {index}: {operation}");
+                    if (firstOperationWasProcessed == false)
+                    {
+                        //Console.Read();
+                        firstOperationWasProcessed = true;
+                    }
+
+
+                }
+                finished = operation.Processed;
+                if (!finished)
+                {
+                    var next = operation.ProcessOperation(currentValue, index);
+
+                    currentValue = next.Item1;
+                    index = next.Item2;
+                    prevIndex = index;
+
+                    if (index >= _operations.Count)
+                    {
+                        Logger.Log($"index : {index}; lastIndex: {prevIndex}");
+                        finished = true;
+                    }
+                    else
+                    {
+                        operation = _operations[index];
+                    }
+
+                }
+
+            } while (finished == false);
+
+            Logger.Log($"index : {index}; lastIndex: {prevIndex}");
+            operation.Print();
+
+            return currentValue;
+        }
+    }
 }
--- AdventOfCode/AdventOfCode/Day_8/Runner.cs
+++ AdventOfCode/AdventOfCode/Day_8/Runner.cs
@@ -6,40 +6,41 @@ using AdventOfCode.Utils;
 
 namespace AdventOfCode.Day_08
 {
-	public class Runner : AbstractRunner
-	{
-		public Runner() : base(8)
-		{
-		}
+    public class Runner : AbstractRunner
+    {
+        public Runner() : base(8)
+        {
+        }
 
-		public IEnumerable<Operation> ParseInput(IEnumerable<string> lines)
-		{
-			return lines.Select(Operation.CreateFromLine); ;
-		}
+        public IEnumerable<Operation> ParseInput(IEnumerable<string> lines)
+        {
+            return lines.Select(Operation.CreateFromLine); ;
+        }
 
-		protected override void Process()
-		{
-			//Part1();
-			Part2();
-		}
+        protected override void Process()
+        {
+            Part1();
+            Part2();
+        }
 
-		private void Part2()
-		{
-			var operations = ParseInput(InputLoader.Instance.LoadInputAsEnumerableOfStrings(Day, "input-part2"));
+        private void Part2()
+        {
+            var operations = ParseInput(InputLoader.Instance.LoadInputAsEnumerableOfStrings(Day, "input-part2"));
 
-			var accumulator = new Accumulator(operations.ToList());
-			var result = accumulator.Process(false);
-			Logger.Log($"Second Part: {result}");
-		}
+            var accumulator = new Accumulator(operations.ToList());
+            var result = accumulator.Process(false);
+            Logger.Log($"Second Part: {result}");
+        }
 
-		private void Part1()
-		{
-			var operations = ParseInput(InputLoader.Instance.LoadInputAsEnumerableOfStrings(Day));
+        private void Part1()
+        {
+            var operations = ParseInput(InputLoader.Instance.LoadInputAsEnumerableOfStrings(Day));
 
-			var accumulator = new Accumulator(operations.ToList());
-			var firstResult = accumulator.Process();
-			Logger.Log($"First Part: {firstResult}");
+            var accumulator = new Accumulator(operations.ToList());
+            var firstResult = accumulator.Process();
+            Logger.Log($"First Part: {firstResult}");
+            Console.Read();
 
-		}
-	}
+        }
+    }
 }
