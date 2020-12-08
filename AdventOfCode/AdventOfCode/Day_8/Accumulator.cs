42cfe04233853efb78d6f8445f1c79b263ec9814
Daniel Auth <daniel.auth@cas.de>
2020-12-08 11:42:51 +0100

    removed commented process function

--- AdventOfCode/AdventOfCode/Day_8/Accumulator.cs
+++ AdventOfCode/AdventOfCode/Day_8/Accumulator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using AdventOfCode.Utils;
 
 namespace AdventOfCode.Day_8
@@ -13,64 +12,7 @@ namespace AdventOfCode.Day_8
             _operations = operations;
         }
 
-        //public int Process(bool skipProcessed = true)
-        //{
-        //	var index = 0;
-        //	int prevIndex = index;
-
-        //	var currentValue = 0;
-        //	var operation = _operations[index];
-        //	var finished = false;
-
-        //	var firstOperationWasProcessed = false;
-
-        //	do
-        //	{
-        //		if (skipProcessed)
-        //		{
-        //			finished = operation.Processed;
-        //		}
-
-
-        //		if (operation.Processed)
-        //		{
-        //			Logger.Log($"First Operation was processed at index {index}: {operation}");
-        //			if (firstOperationWasProcessed == false)
-        //			{
-        //				Console.ReadKey();
-        //				firstOperationWasProcessed = true;
-        //			}
-
-
-        //		}
-
-        //		if (!finished)
-        //		{
-        //			var next = operation.ProcessOperation(currentValue, index);
-
-        //			currentValue = next.Item1;
-        //			prevIndex = index;
-        //			index = next.Item2;
-
-        //			if (index >= _operations.Count)
-        //			{
-        //				Logger.Log($"index : {index}; lastIndex: {prevIndex}");
-        //				finished = true;
-        //			}
-        //			else
-        //			{
-        //				operation = _operations[index];
-        //			}
-
-        //		}
-
-        //	} while (finished == false);
-
-        //	Logger.Log($"index : {index}; lastIndex: {prevIndex}");
-        //	operation.Print();
-
-        //	return currentValue;
-        //}
+        
         public int Process(bool skipProcessed = true)
         {
             var index = 0;
