939f71b56ea4386eb4a12b7096f51f1fe0f76d99
Markus Lind <Markus.Lind@yellowmap.de>
2020-12-09 14:19:33 +0100

    added runners

---
+++ AdventOfCode/AdventOfCode/Day_23/Runner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Day_23
+{
+	public class Runner : AbstractRunner
+	{
+		public Runner() : base(23)
+		{
+		}
+
+		protected override void Process()
+		{
+			Part1();
+			Part2();
+		}
+
+        private void Part1()
+        {
+
+        }
+
+		private void Part2()
+		{
+
+        }
+    }
+}
