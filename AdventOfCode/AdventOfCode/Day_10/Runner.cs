2f4a282e11624d9a62a79f094f6d9afd1b0a391d
Markus Lind <markus.lind@yellowmap.de>
2020-12-10 12:46:07 +0100

    solved day10

--- AdventOfCode/AdventOfCode/Day_10/Runner.cs
+++ AdventOfCode/AdventOfCode/Day_10/Runner.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using AdventOfCode.Utils;
 
@@ -17,14 +19,78 @@ namespace AdventOfCode.Day_10
 			Part2();
 		}
 
-        private void Part1()
-        {
-
-        }
+		private void Part1()
+		{
+			var input = InputLoader.Instance.LoadInputAsEnumerableOfNumbers(Day).ToList();
+			input.Sort();
+			int jumps_1Jolt = 0;
+			int jumps_2Jolt = 0;
+			int jumps_3Jolt = 0;
+			long currentJolt = 0;
+			long deviceJolt = input.Last() + 3;
+			for (int i = 0; i < input.Count; i++)
+			{
+				long adpaterJolt = input[i];
+				var delta = adpaterJolt - currentJolt;
+				switch (delta)
+				{
+					case 1:
+						jumps_1Jolt++;
+						break;
+					case 2:
+						jumps_2Jolt++;
+						break;
+					case 3:
+						jumps_3Jolt++;
+						break;
+					default:
+						throw new System.ArgumentOutOfRangeException();
+				}
+				currentJolt += delta;
+			}
+			jumps_3Jolt++; // letzter Adapter bis Device
+			Logger.Log($"First Part 1-Jolts: {jumps_1Jolt}");
+			Logger.Log($"First Part 3-Jolts: {jumps_3Jolt}");
+			Logger.Log($"First Part: {jumps_1Jolt * jumps_3Jolt}");
+		}
 
 		private void Part2()
 		{
+			var input = InputLoader.Instance.LoadInputAsEnumerableOfNumbers(Day).ToList();
 
-        }
-    }
+			/*
+			input.Clear();
+			input.Add(16);
+			input.Add(10);
+			input.Add(15);
+			input.Add(5);
+			input.Add(1);
+			input.Add(11);
+			input.Add(7);
+			input.Add(19);
+			input.Add(6);
+			input.Add(12);
+			input.Add(4);
+			*/
+
+			input.Add(0);
+			input.Sort();
+			input.Add(input.Last() + 3); // letzter Adapter
+
+			var test = new Dictionary<int, long>();
+			test[input.Count() - 1] = 1; // vorletzter
+
+			for (int i = input.Count() - 2; i >= 0; i--) // rückwärts alle möglichen Adapter
+			{
+				long currentCount = 0;
+				for (var connected = i + 1; connected < input.Count() && input[connected] - input[i] <= 3; connected++)
+				{
+					currentCount += test[connected];
+				}
+				test[i] = currentCount;
+			}
+
+			Logger.Log($"Second Part: {test[0].ToString()}");
+		}
+	}
 }
