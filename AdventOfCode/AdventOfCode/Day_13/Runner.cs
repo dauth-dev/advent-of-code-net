74ba2690cbca9f465dd0e0f9f625beae6990760c
Markus Lind <markus.lind@yellowmap.de>
2020-12-13 16:52:06 +0100

    day13 parr 1

--- AdventOfCode/AdventOfCode/Day_13/Runner.cs
+++ AdventOfCode/AdventOfCode/Day_13/Runner.cs
@@ -19,8 +19,34 @@ namespace AdventOfCode.Day_13
 
         private void Part1()
         {
-
-        }
+			var input = InputLoader.Instance.LoadInputAsEnumerableOfStrings(Day).ToArray();
+			var myNumber = Convert.ToInt32(input[0]);
+			var busLinesTemp = input[1].Split(new char[] { ',' });
+			List<string> busLines = new List<string>();
+			Dictionary<int, int> delays = new Dictionary<int, int>();
+			foreach(var activeLine in busLinesTemp)
+            {
+				if (activeLine == "x")
+				{
+					continue;
+				}
+				busLines.Add(activeLine);
+            }
+			foreach (var activeLine in busLines)
+            {
+				int tempVal = Convert.ToInt32(activeLine);
+				delays.Add(tempVal - myNumber % tempVal, tempVal);
+            }
+			int minDelay = int.MaxValue;
+			foreach(var lineDelay in delays.Keys)
+            {
+				if (lineDelay < minDelay)
+                {
+					minDelay = lineDelay;
+                }
+            }
+			Logger.Log($"First Part: {minDelay * delays[minDelay]}");
+		}
 
 		private void Part2()
 		{
