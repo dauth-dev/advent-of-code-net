95a974e9bcf010f5a1d257c36ff73a1e29fd4941
Markus Lind <markus.lind@yellowmap.de>
2020-12-14 12:16:38 +0100

    finally part 2 ...

--- AdventOfCode/AdventOfCode/Day_14/Runner.cs
+++ AdventOfCode/AdventOfCode/Day_14/Runner.cs
@@ -49,7 +49,9 @@ namespace AdventOfCode.Day_14
 					var bitValue = Convert.ToString(int.Parse(value), 2).PadLeft(36, '0').ToArray();
 
 					for (var i = 0; i < bitmask.Length; i++)
+					{
 						bitValue[i] = bitmask[i] == 'X' ? bitValue[i] : bitmask[i];
+					}
 
 					memValues[memAdress] = string.Join("", bitValue);
 					/*
@@ -69,7 +71,77 @@ namespace AdventOfCode.Day_14
 
 		private void Part2()
 		{
+			var input = InputLoader.Instance.LoadInputAsEnumerableOfStrings(Day).ToList();
+			string bitmask = string.Empty;
 
-        }
-    }
+			/*
+			input.Clear();
+			input.Add("mask = 000000000000000000000000000000X1001X");
+			input.Add("mem[42] = 100");
+			input.Add("mask = 00000000000000000000000000000000X0XX");
+			input.Add("mem[26] = 1");
+			*/
+
+			Dictionary<long, string> memValues = new Dictionary<long, string>();
+			foreach (var line in input)
+			{
+				var temp = line.Split(new char[] { '=' });
+				if (line.StartsWith("mask"))
+				{
+					// neue Maske
+					bitmask = temp[1].Trim();
+				}
+				else if (line.StartsWith("mem"))
+				{
+					var memAdr = temp[0].Trim();
+					int memAdress = Convert.ToInt32(Regex.Replace(memAdr, "[^0-9]", string.Empty));
+					var value = line.Split(" = ")[1];
+					var indexBitValue = Convert.ToString(memAdress, 2).PadLeft(36, '0').ToArray();
+					var bitValue = Convert.ToString(int.Parse(value), 2).PadLeft(36, '0');
+
+					for (int i = 0; i < bitmask.Length; i++)
+						indexBitValue[i] = bitmask[i] == '0' ? indexBitValue[i] : bitmask[i];
+
+					// hole alle die Adressen auf die es Auswirkungen hat
+					var manipulateMemAdr = memAddressesToManipulate(string.Join("", indexBitValue));
+
+					foreach (var temMemAdr in manipulateMemAdr)
+					{
+						memValues[Convert.ToInt64(temMemAdr, 2)] = bitValue;
+					}
+				}
+				else
+				{
+					throw new ArgumentException();
+				}
+			}
+			Logger.Log($"´Second Part: {memValues.Sum(x => Convert.ToInt64(x.Value, 2))}");
+		}
+
+		private static IEnumerable<string> memAddressesToManipulate(string myAdr)
+		{
+			if (!myAdr.Contains("X"))
+			{
+				// keine X => nur eine ADresse
+				return new List<string> { myAdr };
+			}
+			else
+			{
+				// alle X bits durchgehen
+				var memAdr1 = ReplaceFirstOccurence(myAdr, "X", "0");
+				var memAdr2 = ReplaceFirstOccurence(myAdr, "X", "1");
+				return memAddressesToManipulate(memAdr1).Concat(memAddressesToManipulate(memAdr2));
+			}
+		}
+
+		public static string ReplaceFirstOccurence(string text, string search, string replace)
+		{
+			int pos = text.IndexOf(search);
+			if (pos < 0)
+			{
+				return text;
+			}
+			return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+		}
+	}
 }
