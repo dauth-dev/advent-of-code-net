f89b39b1643679871b5bc1dc97d55b1af1df2b0b
Markus Lind <markus.lind@yellowmap.de>
2020-12-17 12:26:57 +0100

    solved day17.2 (ugly copying Part 1 structure...)

--- AdventOfCode/AdventOfCode/Day_17/Runner.cs
+++ AdventOfCode/AdventOfCode/Day_17/Runner.cs
@@ -108,9 +108,97 @@ namespace AdventOfCode.Day_17
             return retList;
         }
 
+        protected static Dictionary<(int x, int y, int z, int w), bool> Neightbors4D((int x, int y, int z, int w) gridCoord, Dictionary<(int x, int y, int z, int w), bool> currentCube)
+        {
+            var retList = new Dictionary<(int x, int y, int z, int w), bool>();
+
+            // Nachbarkoordinaten 3 * 3 * 3 - 1 (Originalwürfel in der Mitte) = 26 Stati
+            for (int i = gridCoord.x - 1; i <= gridCoord.x + 1; i++)
+            {
+                for (int j = gridCoord.y - 1; j <= gridCoord.y + 1; j++)
+                {
+                    for (int k = gridCoord.z - 1; k <= gridCoord.z + 1; k++)
+                    {
+                        for (int l = gridCoord.w - 1; l <= gridCoord.w + 1; l++)
+                        {
+                            if (i == gridCoord.x && j == gridCoord.y && k == gridCoord.z && l == gridCoord.w)
+                            {
+                                // zu prüfende Koordinate ignorieren
+                                continue;
+                            }
+                            else
+                            {
+                                if (currentCube.ContainsKey((i, j, k, l)))
+                                {
+                                    retList.Add((i, j, k, l), currentCube[(i, j, k, l)]);
+                                }
+                                else
+                                {
+                                    // Koordinate außerhalb des Würfels => automatisch false
+                                    retList.Add((i, j, k, l), false);
+                                }
+                            }
+                        } // l (w)
+                    } // k (z)
+                } // j (y)
+            } // i (x)
+            return retList;
+        }
+
         private void Part2()
 		{
+            // aktiv = #, inaktiv = .
+            var input = InputLoader.Instance.LoadInputAsBitMatrix(Day);
 
+            // 4D-Würfel aufbauen x/y/z/w. Das bitARray ist die "unterste" Scheibe
+            var conwayCube = new Dictionary<(int x, int y, int z, int w), bool>();
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[0].Count; j++)
+                {
+                    conwayCube[(i, j, 0, 0)] = input[i][j];
+                }
+            }
+
+            var cubeModified = new Dictionary<(int x, int y, int z, int w), bool>();
+            for (int i = 0; i < 6; i++)
+            {
+                foreach (var cubeItem in conwayCube)
+                {
+                    var itemNeighborStates = Neightbors4D(cubeItem.Key, conwayCube);
+
+                    foreach (var neighbor in itemNeighborStates)
+                    {
+                        // Achtung: die Nachbarn der Nachbarn beeinflussen das Ergebnis
+                        // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active
+                        var neighborsOfNeighbors = Neightbors4D(neighbor.Key, conwayCube).Where(e => conwayCube.ContainsKey(e.Key));
+
+                        if (neighborsOfNeighbors.Count(e => e.Value == true) == 3)
+                        {
+                            cubeModified[neighbor.Key] = true;
+                        }
+                    }
+
+                    // If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active. Otherwise, the cube becomes inactive.
+                    // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active. Otherwise, the cube remains inactive
+                    if (cubeItem.Value == true && (itemNeighborStates.Count(e => e.Value == true) == 2 || itemNeighborStates.Count(e => e.Value == true) == 3))
+                    {
+                        cubeModified[cubeItem.Key] = cubeItem.Value;
+                    }
+                    else if (cubeItem.Value == false && itemNeighborStates.Count(e => e.Value == true) == 3)
+                    {
+                        cubeModified[cubeItem.Key] = true;
+                    }
+                    else
+                    {
+                        cubeModified[cubeItem.Key] = false;
+                    }
+                }
+                // Originalwürfel verändern für den nächsten Durchlauf
+                conwayCube = cubeModified.ToDictionary(e => e.Key, e => e.Value);
+            }
+            int countActiveCubes = conwayCube.Count(e => e.Value == true);
+            Logger.Log($"Second Part: {countActiveCubes}");
         }
     }
 }
