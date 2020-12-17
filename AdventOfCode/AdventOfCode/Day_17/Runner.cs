22c1465ed959c5732fbca7efa1db6babdf31df48
Markus Lind <markus.lind@yellowmap.de>
2020-12-17 12:47:43 +0100

    beautified a bit

--- AdventOfCode/AdventOfCode/Day_17/Runner.cs
+++ AdventOfCode/AdventOfCode/Day_17/Runner.cs
@@ -7,43 +7,49 @@ namespace AdventOfCode.Day_17
 {
 	public class Runner : AbstractRunner
 	{
-		public Runner() : base(17)
+
+        Dictionary<(int x, int y, int z), bool> conwayCube3D = new Dictionary<(int x, int y, int z), bool>();
+        Dictionary<(int x, int y, int z, int w), bool> conwayCube4D = new Dictionary<(int x, int y, int z, int w), bool>();
+        const int loops = 6;
+
+        public Runner() : base(17)
 		{
 		}
 
 		protected override void Process()
 		{
-			Part1();
+            // aktiv = #, inaktiv = .
+            var input = InputLoader.Instance.LoadInputAsBitMatrix(Day);
+            // 3D-Würfel aufbauen x/y/z. Das bitARray ist die unterste Scheibe
+            // 4D-Würfel aufbauen x/y/z. Der 3D-Würfel ist die unterste "Scheibe"
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[0].Count; j++)
+                {
+                    conwayCube3D[(i, j, 0)] = input[i][j];
+                    conwayCube4D[(i, j, 0, 0)] = input[i][j];
+                }
+            }
+
+            Part1();
 			Part2();
 		}
 
         private void Part1()
         {
-			// aktiv = #, inaktiv = .
-			var input = InputLoader.Instance.LoadInputAsBitMatrix(Day);
-
-			// Würfel aufbauen x/y/z. Das bitARray ist die unterste Scheibe
-			var conwayCube = new Dictionary<(int x, int y, int z), bool>();
-			for (int i = 0; i < input.Count; i++)
-            {
-				for (int j = 0; j < input[0].Count; j++)
-                {
-					conwayCube[(i, j, 0)] = input[i][j];
-				}
-			}
-
+            // 3D FAll
             var cubeModified = new Dictionary<(int x, int y, int z), bool>();
-            for (int i=0; i<6;i++)
+            for (int i=0; i < loops; i++)
             {
-                foreach (var cubeItem in conwayCube)
+                foreach (var cubeItem in conwayCube3D)
                 {
-                    var itemNeighborStates = Neightbors3D(cubeItem.Key, conwayCube);
+                    var itemNeighborStates = Neightbors3D(cubeItem.Key, conwayCube3D);
 
                     foreach (var neighbor in itemNeighborStates)
                     {
                         // Achtung: die Nachbarn der Nachbarn beeinflussen das Ergebnis
                         // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active
-                        var neighborsOfNeighbors = Neightbors3D(neighbor.Key, conwayCube).Where(e => conwayCube.ContainsKey(e.Key));
+                        var neighborsOfNeighbors = Neightbors3D(neighbor.Key, conwayCube3D).Where(e => conwayCube3D.ContainsKey(e.Key));
 
                         if (neighborsOfNeighbors.Count(e => e.Value == true) == 3)
                         {
@@ -67,9 +73,9 @@ namespace AdventOfCode.Day_17
                     }
                 }
                 // Originalwürfel verändern für den nächsten Durchlauf
-                conwayCube = cubeModified.ToDictionary(e => e.Key, e => e.Value);
+                conwayCube3D = cubeModified.ToDictionary(e => e.Key, e => e.Value);
             }
-            int countActiveCubes = conwayCube.Count(e => e.Value == true);
+            int countActiveCubes = conwayCube3D.Count(e => e.Value == true);
             Logger.Log($"First Part: {countActiveCubes}");
         }
 
@@ -147,31 +153,19 @@ namespace AdventOfCode.Day_17
 
         private void Part2()
 		{
-            // aktiv = #, inaktiv = .
-            var input = InputLoader.Instance.LoadInputAsBitMatrix(Day);
-
-            // 4D-Würfel aufbauen x/y/z/w. Das bitARray ist die "unterste" Scheibe
-            var conwayCube = new Dictionary<(int x, int y, int z, int w), bool>();
-            for (int i = 0; i < input.Count; i++)
-            {
-                for (int j = 0; j < input[0].Count; j++)
-                {
-                    conwayCube[(i, j, 0, 0)] = input[i][j];
-                }
-            }
-
+            // 4D Fall
             var cubeModified = new Dictionary<(int x, int y, int z, int w), bool>();
             for (int i = 0; i < 6; i++)
             {
-                foreach (var cubeItem in conwayCube)
+                foreach (var cubeItem in conwayCube4D)
                 {
-                    var itemNeighborStates = Neightbors4D(cubeItem.Key, conwayCube);
+                    var itemNeighborStates = Neightbors4D(cubeItem.Key, conwayCube4D);
 
                     foreach (var neighbor in itemNeighborStates)
                     {
                         // Achtung: die Nachbarn der Nachbarn beeinflussen das Ergebnis
                         // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active
-                        var neighborsOfNeighbors = Neightbors4D(neighbor.Key, conwayCube).Where(e => conwayCube.ContainsKey(e.Key));
+                        var neighborsOfNeighbors = Neightbors4D(neighbor.Key, conwayCube4D).Where(e => conwayCube4D.ContainsKey(e.Key));
 
                         if (neighborsOfNeighbors.Count(e => e.Value == true) == 3)
                         {
@@ -195,9 +189,9 @@ namespace AdventOfCode.Day_17
                     }
                 }
                 // Originalwürfel verändern für den nächsten Durchlauf
-                conwayCube = cubeModified.ToDictionary(e => e.Key, e => e.Value);
+                conwayCube4D = cubeModified.ToDictionary(e => e.Key, e => e.Value);
             }
-            int countActiveCubes = conwayCube.Count(e => e.Value == true);
+            int countActiveCubes = conwayCube4D.Count(e => e.Value == true);
             Logger.Log($"Second Part: {countActiveCubes}");
         }
     }
