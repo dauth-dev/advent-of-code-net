7c0f056af1b8efa80f8db7cb29f44c1821f9d6cf
Markus Lind <markus.lind@yellowmap.de>
2020-12-19 16:31:49 +0100

    solved day 19.2

--- AdventOfCode/AdventOfCode/Day_19/Runner.cs
+++ AdventOfCode/AdventOfCode/Day_19/Runner.cs
@@ -8,12 +8,35 @@ namespace AdventOfCode.Day_19
 {
 	public class Runner : AbstractRunner
 	{
+		List<string> input;
+		Dictionary<int, string> rules = new Dictionary<int, string>();
+		List<string> messages = new List<string>();
+
 		public Runner() : base(19)
 		{
 		}
 
 		protected override void Process()
 		{
+			input = InputLoader.Instance.LoadInputAsEnumerableOfStrings(Day).ToList();
+			var isRule = true;
+			foreach (var line in input)
+			{
+				if (string.IsNullOrEmpty(line))
+				{
+					isRule = false;
+					continue;
+				}
+				if (isRule)
+				{
+					rules.Add(Convert.ToInt32(line.Split(new char[] { ':' })[0]), line.Split(new char[] { ':' })[1].Replace("\"", string.Empty));
+				}
+				else
+				{
+					messages.Add(line);
+				}
+			}
+
 			Part1();
 			Part2();
 		}
@@ -41,26 +64,6 @@ Some of the rules have multiple lists of sub-rules separated by a pipe (|). This
 			 * 
 			 * */
 
-			var input = InputLoader.Instance.LoadInputAsEnumerableOfStrings(Day).ToList();
-			var isRule = true;
-			Dictionary<int, string> rules = new Dictionary<int, string>();
-			List<string> messages = new List<string>();
-			foreach(var line in input)
-            {
-				if (string.IsNullOrEmpty(line))
-                {
-					isRule = false;
-					continue;
-                }
-				if (isRule)
-                {
-					rules.Add(Convert.ToInt32(line.Split(new char[] { ':' })[0]), line.Split(new char[] { ':' })[1].Replace("\"", string.Empty));
-                }
-				else
-                {
-					messages.Add(line);
-                }
-            }
 
 			// Your goal is to determine the number of messages that completely match rule 0.
 			
@@ -69,6 +72,49 @@ Some of the rules have multiple lists of sub-rules separated by a pipe (|). This
 			// Zahlen verweisen auf andere Regex. Diese müssen als Gruppe übersetzt werden
 			// die | in den Regelen können 1:1 in den RegEx übernommen werden als Gruppe (min 1 muss gematcht werdeb)
 
+			Logger.Log($"First Part: {countMatches()}");
+		}
+
+		private void Part2()
+		{
+			/*
+			 * As you look over the list of messages, you realize your matching rules aren't quite right. 
+			 * To fix them, completely replace rules 8: 42 and 11: 42 31 with the following:
+8: 42 | 42 8
+11: 42 31 | 42 11 31
+This small change has a big impact: now, the rules do contain loops, and the list of messages they could hypothetically 
+			match is infinite. You'll need to determine how these changes affect which messages are valid.
+			*/
+
+			rules[8] = "42 | 42 8";
+			rules[11] = "42 31 | 42 11 31";
+
+			/*
+			 *
+			 * Fortunately, many of the rules are unaffected by this change; it might help to start by looking at which rules
+			 * always match the same set of values and how those rules (especially rules 42 and 31) 
+			 * are used by the new versions of rules 8 and 11
+			 * 
+			 * 
+			 * => 42 und 31 kommen nur in den beiden Regeln vor
+			 * die neue R8 besagt dann  dass R42 1 16 | 47 26 rekursiv wird
+			 * aus R11: 42 31 wird 42 11 31 was bedeutet 42 42 31 31
+			 * wird zu ( 1 16 | 47 26 ) (1 16 |47 26) (83 26 | 90 16) 
+			 * wird zu ( 1 b | 47 26) ( 1 b | 47 26) ( 83 26 | 90 16)
+			 * ...
+			 * */
+
+			// die längste Nachricht waren 97 Zeichen
+			// mit den Testdaten hat es gereicht das auf 4 fach aufzublährn statt rekursiv. Später ggf. generalisieren
+			rules[8] = "42 | 42 (42 | 42 (42 | 42 (42 | 42 (42))))";
+			rules[11] = "42 31 | 42 (42 31 | 42 (42 31 | 42 (42 31 | 42 31) 31) 31) 31";
+
+			
+			Logger.Log($"Second Part: {countMatches()}");
+		}
+
+		private int countMatches()
+        {
 			var test = rules[0];
 			while (Regex.IsMatch(test, @"\d+"))
 			{
@@ -76,22 +122,17 @@ Some of the rules have multiple lists of sub-rules separated by a pipe (|). This
 			}
 			test = test.Replace(" ", string.Empty).Trim();
 
-			// Ergibt ein Regex-Monster mit 4302 Zeichen ... 
+			// Ergibt (in Part 1) ein Regex-Monster mit 4302 Zeichen ... 
 			var regexForTesting = new Regex("^" + test + "$");
 			var counter = 0;
-			foreach(var msg in messages)
-            {
+			foreach (var msg in messages)
+			{
 				if (regexForTesting.IsMatch(msg))
-                {
+				{
 					counter++;
-                }
-            }
-			Logger.Log($"First Part: {counter}");
+				}
+			}
+			return counter;
 		}
-
-		private void Part2()
-		{
-
-        }
     }
 }
