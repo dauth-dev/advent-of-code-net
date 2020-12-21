622e0ea12903c1a47d269e22f8c5bff737f44fdc
Markus Lind <markus.lind@yellowmap.de>
2020-12-21 16:00:41 +0100

    refactor load

--- AdventOfCode/AdventOfCode/Day_21/Runner.cs
+++ AdventOfCode/AdventOfCode/Day_21/Runner.cs
@@ -8,13 +8,17 @@ namespace AdventOfCode.Day_21
 	public class Runner : AbstractRunner
 	{
 		List<string> input;
-		public Runner() : base(21)
+        Dictionary<string, int> ingredientCount = new Dictionary<string, int>();
+        Dictionary<string, HashSet<string>> allergenMapping = new Dictionary<string, HashSet<string>>();
+
+        public Runner() : base(21)
 		{
 		}
 
 		protected override void Process()
 		{
 			input = InputLoader.Instance.LoadInputAsEnumerableOfStrings(Day).ToList();
+            LoadData();
 			Part1();
 			Part2();
 		}
@@ -46,44 +50,6 @@ The first step is to determine which ingredients can't possibly contain any of t
 			 */
 
 
-            Dictionary<string, int> ingredientCount = new Dictionary<string, int>();
-            Dictionary<string, HashSet<string>> allergenMapping = new Dictionary<string, HashSet<string>>();
-
-            foreach (var line in input)
-            {
-                var ingredient = line.Substring(0, line.IndexOf("(")).Trim(new char[] { '(', ' ' });
-                var allergen = line.Substring(line.IndexOf("(") +1, line.Length - (line.IndexOf("(") + 1)).Replace(")", string.Empty).Trim();
-                allergen = allergen.Replace("contains ", string.Empty);
-                
-                var singleIngredients = ingredient.Split(" ").ToList();
-                var allergenList = allergen.Split(", ").ToList();
-
-
-                foreach(var ing in singleIngredients)
-                {
-                    if (ingredientCount.ContainsKey(ing))
-                    {
-                        ingredientCount[ing] += 1;
-                    }
-                    else
-                    {
-                        ingredientCount[ing] = 1;
-                    }
-                }
-
-                foreach(var alg in allergenList)
-                {
-                    if (allergenMapping.ContainsKey(alg))
-                    {
-                        allergenMapping[alg].IntersectWith(singleIngredients);
-                    }
-                    else
-                    {
-                        allergenMapping[alg] = new HashSet<string>(singleIngredients);
-                    }
-                }
-
-            }
 
             var alList = allergenMapping.Values.SelectMany(x => x).ToList();
             
@@ -97,7 +63,7 @@ The first step is to determine which ingredients can't possibly contain any of t
                     if (!alList.Contains(x))
                     {
                         allergenCount += ingredientCount[x];
-                        ingredientCount.Remove(x);
+                        // ingredientCount.Remove(x);
                     }
                 }
             // }
@@ -108,7 +74,52 @@ The first step is to determine which ingredients can't possibly contain any of t
         }
         private void Part2()
 		{
+            /*
+             * 
+             * Arrange the ingredients alphabetically by their allergen and separate them by commas to produce
+             * your canonical dangerous ingredient list. (There should not be any spaces in your canonical dangerous 
+             * ingredient list.) In the above example, this would be mxmxvkd,sqjhc,fvjkl.    
+             * 
+             * */
+        }
 
+        private void LoadData()
+        {
+            foreach (var line in input)
+            {
+                var ingredient = line.Substring(0, line.IndexOf("(")).Trim(new char[] { '(', ' ' });
+                var allergen = line.Substring(line.IndexOf("(") + 1, line.Length - (line.IndexOf("(") + 1)).Replace(")", string.Empty).Trim();
+                allergen = allergen.Replace("contains ", string.Empty);
+
+                var singleIngredients = ingredient.Split(" ").ToList();
+                var allergenList = allergen.Split(", ").ToList();
+
+
+                foreach (var ing in singleIngredients)
+                {
+                    if (ingredientCount.ContainsKey(ing))
+                    {
+                        ingredientCount[ing] += 1;
+                    }
+                    else
+                    {
+                        ingredientCount[ing] = 1;
+                    }
+                }
+
+                foreach (var alg in allergenList)
+                {
+                    if (allergenMapping.ContainsKey(alg))
+                    {
+                        allergenMapping[alg].IntersectWith(singleIngredients);
+                    }
+                    else
+                    {
+                        allergenMapping[alg] = new HashSet<string>(singleIngredients);
+                    }
+                }
+
+            }
         }
     }
 }
